53dda6ffc1433d24226a8f0df26fd0da11065847
alena <mittralena@gmail.com>
2020-05-13 12:25:41 +0400

    add email support

--- app/models.py
+++ app/models.py
@@ -1,7 +1,10 @@
 from datetime import datetime
-from app import db, login
+from app import db, login, app
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from time import time
+import jwt
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -18,6 +21,21 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
+
 
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- app/routes.py
+++ app/routes.py
@@ -1,28 +1,26 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, NewRecipeForm, ResetPasswordRequestForm, ResetPasswordForm
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User
+from app.models import User, Recipe
 from werkzeug.urls import url_parse
+from app.email import send_password_reset_email
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    recipes = [
-        {
-            'author': {'username': 'Alena'},
-            'title': 'Cake',
-            'description': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Alena'},
-            'title': 'Soup',
-            'description': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', recipes=recipes)
+    form = NewRecipeForm()
+    if form.validate_on_submit():
+        recipe = Recipe(title=form.title.data, author=current_user)
+        db.session.add(recipe)
+        db.session.commit()
+        flash('Your recipe has been saved!')
+        return redirect(url_for('index'))
+    recipes = current_user.recipes
+
+    return render_template('index.html', title='Home', form=form, recipes=recipes)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -88,3 +86,34 @@ def edit_profile():
         form.username.data = current_user.username
     return render_template('edit_profile.html', title='Edit Profile',
                            form=form)
+
+
+@app.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_password_reset_email(user)
+        flash('Check your email for the instructions to reset your password')
+        return redirect(url_for('login'))
+    return render_template('reset_password_request.html',
+                           title='Reset Password', form=form)
+
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    user = User.verify_reset_password_token(token)
+    if not user:
+        return redirect(url_for('index'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user.set_password(form.password.data)
+        db.session.commit()
+        flash('Your password has been reset.')
+        return redirect(url_for('login'))
+    return render_template('reset_password.html', form=form)
