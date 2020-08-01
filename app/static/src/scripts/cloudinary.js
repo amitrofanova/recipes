cb7cbf3d244d6b896a80078aa0ab9945cc307b73
alena <mittralena@gmail.com>
2020-08-01 16:04:02 +0400

    WIP adding cloudinary images

--- app/static/src/scripts/cloudinary.js
+++ app/static/src/scripts/cloudinary.js
@@ -1,43 +1,44 @@
-function uploadImg(e) {
+function uploadImg() {
     const cloudName = 'dzhpku2a7';
     const unsignedUploadPreset = 'tmrndy21';
     let url = `https://api.cloudinary.com/v1_1/${cloudName}/upload`;
 
-    const file = e.target.files[0];
-    console.log(file);
+    let file = document.querySelector("#fileSelect").files[0];
+
     const formData = new FormData();
-
-    let img = document.querySelector("#fileSelect").files[0];
-        console.log(img);
-
     formData.append('file', file);
     formData.append('upload_preset', unsignedUploadPreset);
 
-    // fetch(url, {
-    //     method: 'POST',
-    //     body: formData,
-    // })
-    // .then(response => response.json())
-    // .then((data) => {
-    //   if (data.secure_url !== '') {
-    //     const uploadedFileUrl = data.secure_url;
-    //
-    //     document.querySelector("#cl_img").setAttribute("src", uploadedFileUrl);
-    //   }
-    // })
-    // .catch(err => console.error(err));
+    fetch(url, {
+        method: 'POST',
+        body: formData,
+    })
+    .then(response => response.json())
+    .then((data) => {
+      if (data.secure_url !== '') {
+          const uploadedFileUrl = data.secure_url;
+
+        // document.querySelector("#cl_img").setAttribute("src", uploadedFileUrl);
+          // document.getElementById("addRecipeForm").submit();
+          document.createElement('form').submit.call(document.addRecipeForm);
+      }
+    })
+    .catch(err => console.error(err));
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const fileSelect = document.querySelector("#fileSelect");
+    if (window.location.pathname.includes("modify_recipe") == false) {
+        console.log("not modify_recipe"); return;
+    }
+    // const fileSelect = document.querySelector("#fileSelect");
     const addRecipeForm = document.querySelector("#addRecipeForm");
 
-    fileSelect.addEventListener('change', (e) => {
-        uploadImg(e);
-    });
+    // fileSelect.addEventListener('change', (e) => {
+    //     uploadImg(e);
+    // });
 
-    // addRecipeForm.addEventListener('submit', (e) => {
-    //     let img = document.querySelector("#fileSelect").files[0];
-    //     console.log(img);
-    // })
+    addRecipeForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        uploadImg();
+    })
 });
