671d9f6c43b2e2cea7ae74576e562cb3839baa42
alena <mittralena@gmail.com>
2020-08-09 12:49:39 +0400

    refactor

---
+++ app/static/src/scripts/ideas.js
@@ -0,0 +1,26 @@
+async function deleteIdea(event) {
+    let selectedIdeaNum = event.target.getAttribute("data-id");
+    let url = window.location.pathname;
+
+    let response = await fetch(url, {
+        method: 'DELETE',
+        body: selectedIdeaNum
+    });
+
+    if (response.ok) {
+        location.reload();
+    } else {
+        alert("Ошибка HTTP: " + response.status);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    let deleteIdeaBtns = document.querySelectorAll("#ideas button");
+
+    Array.from(deleteIdeaBtns).forEach((btn) => {
+        btn.addEventListener("click", () => {
+                deleteIdea(event);
+            }
+        );
+    });
+});
--- app/static/src/scripts/scripts.js
+++ app/static/src/scripts/scripts.js
@@ -1,28 +1,3 @@
 import '../styles/app.scss';
-
-async function deleteIdea(event) {
-    let selectedIdeaNum = event.target.getAttribute("data-id");
-    let url = window.location.pathname;
-
-    let response = await fetch(url, {
-        method: 'DELETE',
-        body: selectedIdeaNum
-    });
-
-    if (response.ok) {
-        location.reload();
-    } else {
-        alert("Ошибка HTTP: " + response.status);
-    }
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    let deleteIdeaBtns = document.querySelectorAll("#ideas button");
-
-    Array.from(deleteIdeaBtns).forEach((btn) => {
-        btn.addEventListener("click", () => {
-                deleteIdea(event);
-            }
-        );
-    });
-});
+import './ideas.js';
+import './cloudinary.js';
