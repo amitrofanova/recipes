a60971ba29cf6d6abdf01323762ff0025144499c
alena <mittralena@gmail.com>
2020-08-10 18:17:37 +0400

    style "fileSelect" button

--- app/static/src/scripts/cloudinary.js
+++ app/static/src/scripts/cloudinary.js
@@ -28,11 +28,16 @@ document.addEventListener('DOMContentLoaded', () => {
     if (!window.location.pathname.includes("modify_recipe") &&
         !window.location.pathname.includes("add_recipe")) return;
 
+    const fileSelect = document.querySelector('#fileSelect');
+    fileSelect.addEventListener('change', (e) => {
+        document.querySelector('#fileName').innerHTML = e.target.files[0].name;
+    });
+
     const addRecipeForm = document.querySelector('#addRecipeForm');
     addRecipeForm.addEventListener('submit', (e) => {
         e.preventDefault();
         processImg();
-    })
+    });
 
     if (window.location.pathname.includes("modify_recipe")) {
         let imgUrl = document.querySelector("#image_url").value;
